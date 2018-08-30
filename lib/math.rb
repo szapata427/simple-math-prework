1833f16bb4710a659fe11871b683a71391f65e1a
Steven Zapata <szapata427@gmail.com>
2018-08-30 19:37:41 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,16 +7,19 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  num1 + num2
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 % num2
 end
 
 def square_root(num)
+  num.square_root
 end
 
 def order_of_operation(num1, num2, num3, num4)
